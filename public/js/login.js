81c8bcb58ceddbe58eeceeba8c2c1e4f00ba3f12
killiankraus <44647842+killiankraus@users.noreply.github.com>
2022-01-14 00:07:20 +0800

    first commit test login

---
+++ public/js/login.js
@@ -0,0 +1,43 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyApN_UInVP2mV0jKbsdAeRPUCXDYPZuCwU",
+  authDomain: "iot-aeroponics.firebaseapp.com",
+  databaseURL: "https://iot-aeroponics-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "iot-aeroponics",
+  storageBucket: "iot-aeroponics.appspot.com",
+  messagingSenderId: "685018331718",
+  appId: "1:685018331718:web:cdb49e6a4d29a2f2372847",
+  measurementId: "G-WCYJ0TPNEX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+document.getElementById("signin").addEventListener('click', function(){
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+        console.log("log in");
+        location.href = "index.html";
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        console.log(errorCode + errorMessage);
+    });  
+})
+
